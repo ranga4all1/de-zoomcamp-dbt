201d2590772fd7f7c2b8fa6d71a25442880062bb
r h <ranga4all1.ai5@gmail.com>
2024-02-21 04:19:14 +0000

    added fhv

---
+++ taxi_rides_ny_1/models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,37 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
+  from {{ source('staging','fhv_tripdata') }}
+  where dispatching_base_num is not null 
+)
+select
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    {{ dbt.safe_cast("dispatching_base_num", api.Column.translate_type("string")) }} as dispatching_base_num,
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    CAST(SR_Flag as string) as sr_flag,
+    CAST(Affiliated_base_number as string) as affiliated_base_number,
+from tripdata
+where rn = 1 
+    AND EXTRACT(YEAR FROM pickup_datetime) = 2019
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
